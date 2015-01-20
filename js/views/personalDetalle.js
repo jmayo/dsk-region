10cafad83493e417a2e165378d1b86ffe05854db
Raul Torres <raultr@gmail.com>
2015-01-19 23:52:24 -0600

    helper para los radio buttons

--- js/views/personalDetalle.js
+++ js/views/personalDetalle.js
@@ -30,10 +30,26 @@ Personal.Views.PersonalDetalle = Backbone.View.extend({
           var visEscolaridad = new Personal.Views.PersonalCatalogos({
             collection: catEscolaridad,cdu_seleccionado: detalle["cdu_escolaridad"] ,id_select: "#perso_escolaridad"});
           visEscolaridad.render();
+          
           var catReligion = new Backbone.Collection(PersonalCatalogos.Religion());
           var visReligion = new Personal.Views.PersonalCatalogos({
             collection: catReligion,cdu_seleccionado: detalle["cdu_religion"] ,id_select: "#perso_religion" });
           visReligion.render();
+          
+          var catEdoNac = new Backbone.Collection(PersonalCatalogos.Estados());
+          var visEdoNac = new Personal.Views.PersonalCatalogos({
+            collection: catEdoNac,cdu_seleccionado: detalle["cdu_estado_nac"] ,id_select: "#perso_edonac" });
+          visEdoNac.render();
+
+          var catSegSoc = new Backbone.Collection(PersonalCatalogos.SeguridadSocial());
+          var visSegSoc = new Personal.Views.PersonalCatalogos({
+            collection: catSegSoc,cdu_seleccionado: detalle["cdu_seguridad_social"] ,id_select: "#perso_segsoc" });
+          visSegSoc.render();
+        
+          var catEdoCiv = new Backbone.Collection(PersonalCatalogos.EstadoCivil());
+          var visEdoCiv = new Personal.Views.PersonalCatalogos({
+            collection: catEdoCiv,cdu_seleccionado: "" ,id_select: "#perso_estado_civil" });
+          visEdoCiv.render();
         }
     });
   }
