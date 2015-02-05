a6125345583e0de0cf5340b5dc90ec15bf79c15a
jmayo <jcmmayo@gmail.com>
2015-02-04 22:10:59 -0600

    marcador en mapa

--- js/mapa.js
+++ js/mapa.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+var pos_ini = -99.1696;
+var pos_fin = 19.5225;
+
 var map = new ol.Map({
 	target: 'mapa',
 	layers: [
@@ -7,9 +10,24 @@ var map = new ol.Map({
 		})
 	],
 	view: new ol.View({
-		center: ol.proj.transform([131.044922, -25.363882], 'EPSG:4326', 'EPSG:3857'),
-		zoom: 1
+		center: ol.proj.transform([pos_ini, pos_fin], 'EPSG:4326', 'EPSG:3857'),
+		zoom: 14
 	})
 });
+
+map.addOverlay(new ol.Overlay({
+	position: ol.proj.transform(
+		[pos_ini, pos_fin],
+		'EPSG:4326',
+		'EPSG:3857'
+	),
+	//element: $('<img src="images/marker.png" height=20px weight=20px>')
+
+element: $('<img class="location-popover" src="images/marker.png" height=20px weight=20px>')
+
+
+}));
+
+
 });
 
