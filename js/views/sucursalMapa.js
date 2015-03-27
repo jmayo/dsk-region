c76610f2b54fac7e88839250048798aa47fdfb40
Raul Torres <raultr@gmail.com>
2015-03-26 22:31:41 -0600

    imagen para cuando ubica mi posicion

--- js/views/sucursalMapa.js
+++ js/views/sucursalMapa.js
@@ -25,8 +25,10 @@ Personal.Views.SucursalMapa = Backbone.View.extend({
         var self=this;
         navigator.geolocation.getCurrentPosition(function(position){
             console.log(position);
-             self.posicionar(position.coords.longitude,position.coords.latitude,true)
+             self.posicionar(position.coords.latitude,position.coords.longitude,true);
+             self.marcaTemporal(position.coords.latitude,position.coords.longitude);
         });
+
     } 
     else {
        $("#notify_warning").notify();
@@ -35,52 +37,81 @@ Personal.Views.SucursalMapa = Backbone.View.extend({
   marcar: function(id,latitud,longitud){
     console.log("poner marca");
     
-    var pos_ini = parseFloat(latitud);
-    var pos_fin = parseFloat(longitud);
+    var lon = parseFloat(longitud);
+    var lat = parseFloat(latitud);
     
-    this.map.latitud = pos_ini;
-    this.map.longitud = pos_fin;
+    this.map.latitud = lat;
+    this.map.longitud = lon;
     
     var self= this;
     //Si ya existe una marca para ese servicio la quitamos del mapa
    
     var imagen = new ol.Overlay({
           position: ol.proj.transform(
-            [this.map.latitud, this.map.longitud],
-            'EPSG:3857',
+            [ this.map.longitud,this.map.latitud],
             'EPSG:4326',
+            'EPSG:3857'
           ),
        element: $('<img src="images/marker.png" height=20px weight=20px>')
        });
 
   if(this.marcas[id]){
       this.map.removeOverlay(this.marcas[id]);
-      this.posicionar(this.map.latitud, this.map.longitud);
+      this.posicionar( this.map.longitud,this.map.latitud);
   } 
    this.map.addOverlay(imagen);
    
    this.marcas[id] =  imagen;
+  },
+  marcaTemporal: function(latitud,longitud){
+    console.log("poner marca");
+    
+    var lon = parseFloat(longitud);
+    var lat = parseFloat(latitud);
+    
+    this.map.latitud = lat;
+    this.map.longitud = lon;
+    
+    var self= this;
+    //Si ya existe una marca para ese servicio la quitamos del mapa
+   
+    var imagen = new ol.Overlay({
+          position: ol.proj.transform(
+            [ this.map.longitud,this.map.latitud],
+            'EPSG:4326',
+            'EPSG:3857'
+          ),
+       element: $('<img src="images/ubicacion.png" height=50px weight=50px>')
+       });
+
+  if(this.marcas["marca_temporal"]){
+      this.map.removeOverlay(this.marcas["marca_temporal"]);
+   //   this.posicionar( this.map.longitud,this.map.latitud);
+  } 
+   this.map.addOverlay(imagen);
+   
+   this.marcas["marca_temporal"] =  imagen;
   },
   zoom: function(valor){
     this.map.getView().setZoom(valor);
 
   },
   posicionar: function(latitud,longitud,cambiar){
-    var pos_ini = (parseFloat(latitud).toFixed(4))/1;
-    var pos_fin = (parseFloat(longitud).toFixed(4))/1;
-    this.map.latitud = pos_ini;
-    this.map.longitud = pos_fin;
-    this.map.getView().setCenter(ol.proj.transform([this.map.latitud, this.map.longitud], 'EPSG:3857','EPSG:4326'));
+    var lat = (parseFloat(latitud).toFixed(4))/1;
+    var lon = (parseFloat(longitud).toFixed(4))/1;
+    this.map.latitud = lat;
+    this.map.longitud = lon;
+    this.map.getView().setCenter(ol.proj.transform([this.map.longitud,this.map.latitud],'EPSG:4326', 'EPSG:3857'));
     this.zoom(14);
     if(cambiar===true){
-        $('#sucursal_latitud').val(pos_ini);
-        $('#sucursal_longitud').val(pos_fin);
+        $('#sucursal_latitud').val(lat);
+        $('#sucursal_longitud').val(lon);
     }
   },
   mostrarMapa: function(latitud,longitud){
         $('#mapa').empty();
-        pos_ini= parseFloat(latitud);
-        pos_fin = parseFloat(longitud);
+        lat= parseFloat(latitud);
+        lon = parseFloat(longitud);
         this.map = new ol.Map({
           target: 'mapa',
           layers: [
@@ -89,7 +120,7 @@ Personal.Views.SucursalMapa = Backbone.View.extend({
             })
           ],
           view: new ol.View({
-            center: ol.proj.transform([pos_ini, pos_fin],'EPSG:3857''EPSG:4326'),
+            center: ol.proj.transform([ lon,lat],'EPSG:4326','EPSG:3857'),
             zoom: 12
           })
         });
